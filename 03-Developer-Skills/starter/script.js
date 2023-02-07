59d56fe38918fe0d9d5cbb1b1aa21d0b7fe86d2e
casparcrop94 <casparcrop@gmail.com>
2023-02-07 17:17:05 +0100

    Forecast

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,14 @@
-// Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const data_1 = [17, 21, 23];
+const data_2 = [12, 5, -5, 0, 4];
+
+const printForecast = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`${arr[i]}ºC in ${i + 1} days.`);
+  }
+};
+
+printForecast(data_1);
+console.log("");
+printForecast(data_2);
