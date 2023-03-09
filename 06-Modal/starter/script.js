4f557454e67e2eb29faf7fa6726f1b59bc12f5c1
casparcrop94 <casparcrop@gmail.com>
2023-03-10 00:31:27 +0100

    Finished Modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -3,23 +3,28 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnOpenModal = document.querySelectorAll('.show-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 const openModal = function () {
-  console.log('Modal Opened');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
 const closeModal = function () {
-  console.log('Modal Closed');
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
-btnCloseModal.addEventListener('click', closeModal());
-overlay.addEventListener('click', closeModal());
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+};
 
-for (let i = 0; i < btnOpenModal.length; i++) {
-  btnOpenModal[i].addEventListener('click', openModal());
-}
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function(e){
+  // console.log(e);
+  if(e.key === 'Escape' && !modal.classList.contains('hidden')){
+    closeModal();
+  }
+});
