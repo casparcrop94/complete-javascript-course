5b23daacd020dbfe26cba8b30c4f6f55eb2ec17a
casparcrop94 <casparcrop@gmail.com>
2023-03-17 16:30:49 +0100

    Working BTS

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2023 - birthYear;
+  //   console.log(firstName);
+  function printAge() {
+    const output = `${firstName} is ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millennial = true;
+      const str = `Oh, you're a millennial, ${firstName}`;
+      console.log(str);
+      console.log(millennial);
+    }
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Caspar';
+calcAge(1994);
+
+// console.log(age);
+// printAge();
